4ca6aabe9521014d870921077ec6e1114d53f656
Caden Parker <cadenparker@ymail.com>
2021-02-27 19:50:00 -0800

    Update Vulkan SDK

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-clang -g main.c -o build\a.exe -IC:\VulkanSDK\1.2.154.1\Include\vulkan -lUser32.lib -lC:\VulkanSDK\1.2.154.1\Lib\vulkan-1.lib
+clang -g main.c -o build\a.exe -IC:\VulkanSDK\1.2.162.1\Include\vulkan -lUser32.lib -lC:\VulkanSDK\1.2.162.1\Lib\vulkan-1.lib
